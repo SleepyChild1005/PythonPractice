b68e9990fa52f9becb8c3e4368f400a16ea09670
kangdong <tarky0417@gmail.com>
2021-08-28 19:50:32 +0900

    pytorch practice 1

--- PyTorchPractice.py
+++ PyTorchPractice.py
@@ -9,8 +9,46 @@ b = torch.tensor(1)
 
 print(a)
 print(b)
+print(type(a), type(b))
 
+# Numpy initializing two arrays
+a = np.array(2)
+b = np.array(1)
+print(a,b)
 
+# addition
+print(a+b)
+
+# subtraction
+print(b-a)
+
+# multiplication
+print(a*b)
+
+# division
+print(a/b)
+
+# Torch initializing two tensors
+a = torch.tensor(2)
+b = torch.tensor(1)
+print(a,b)
+
+# addition
+print(a+b)
+
+# subtraction
+print(b-a)
+
+# multiplication
+print(a*b)
+
+# division
+print(a/b)
+
+# matrix of zeros
+a = np.zeros((3,3))
+print(a)
+print(a.shape)
 
 
 
