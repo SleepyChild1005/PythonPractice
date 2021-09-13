9f1149ae5dbad94e2160563e1f16d42d789b6c48
kangdong <tarky0417@gmail.com>
2021-09-13 19:59:59 +0900

    pytorch practice
    from scratch

--- TensorFlowPractice.py
+++ TensorFlowPractice.py
@@ -92,7 +92,29 @@ matplotlib_imshow(img_grid, one_channel=True)
 # write to tensorboard
 writer.add_image('four_fashion_mnist_images', img_grid)
 
+writer.add_graph(net, images)
+writer.close()
 
+# helper function
+def select_n_random(data, labels, n=100):
+    '''
+    Selects n random datapoints and their corresponding labels from a dataset
+    '''
+    assert len(data) == len(labels)
 
+    perm = torch.randperm(len(data))
+    return data[perm][:n], labels[perm][:n]
 
+# select random images and their target indices
+images, labels = select_n_random(trainset.data, trainset.targets)
+
+# get the class labels for each image
+class_labels = [classes[lab] for lab in labels]
+
+# log embeddings
+features = images.view(-1, 28 * 28)
+writer.add_embedding(features,
+                    metadata=class_labels,
+                    label_img=images.unsqueeze(1))
+writer.close()
 
